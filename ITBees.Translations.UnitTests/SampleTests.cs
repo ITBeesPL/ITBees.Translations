bc6f3e24c8d800d7d839af3126f808efd6c6b41b
Jakub Zohar Rutkowski <kuba@skb.pl>
2023-02-15 14:47:12 +0100

    Add project files.

---
+++ ITBees.Translations.UnitTests/SampleTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ITBees.Translations.UnitTests
+{
+    public class SampleTests
+    {
+        [Test]
+        public void SampleTestShouldReturnTrue()
+        {
+            Assert.True(new ITBees.Translations.Translation() != null);
+        }
+    }
+}
---
+++ ITBees.Translations/Translation.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ITBees.Translations
+{
+    public class Translation
+    {
+
+    }
+}
