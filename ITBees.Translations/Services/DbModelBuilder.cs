2efe7058f5f3d6debbf9bdfa28c99fb11ba0a2dc
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-09-26 18:24:10 +0200

    translations with unique index

--- ITBees.Translations/Services/DbModelBuilder.cs
+++ ITBees.Translations/Services/DbModelBuilder.cs
@@ -1,12 +1,14 @@
 ﻿using ITBees.Translations.SqlMigration;
+using Microsoft.EntityFrameworkCore;
 
 namespace ITBees.Translations.Services
 {
     public class DbModelBuilder
     {
-        public static void Register(dynamic modelBuilder)
+        public static void Register(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<RuntimeTranslation>();
+            modelBuilder.Entity<RuntimeTranslation>().HasIndex(x => new {x.BasePhraseId, x.LanguageId}).IsUnique();
+            modelBuilder.Entity<BasePhrase>().HasIndex(x => x.Phrase).IsUnique();
         }
     }
 }
